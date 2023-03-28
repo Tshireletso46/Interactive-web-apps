49b5bc931b4dfa3bcc7ad3bf7fbbe085770c286b
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-03-28 09:33:06 +0200

    IWA1_BCL2302_GroupOwen_TshireletsoMpudu

--- scripts.js
+++ scripts.js
@@ -1,32 +1,29 @@
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
-const STEP_AMOUNT = 5
 
-const number= document. querySelector('[data-key="mumber"]')
-const subtract=document. querySelector('[data-key="substract"]')
-const add=document. querySelector('[data-key="add"]')
+const number = document.querySelector('[data-key="number"]')
+const add =  document.querySelector('[data-key="add"]')
+const subtract =  document.querySelector('[data-key="subtract"]')
 
-const substractHandler = () =>{
-    const newValue= parseInt(number.value) - STEP_AMOUNT
-    number.value = newValue
-
-    if (add. disabled === true) {
-        add.disabled === false
-    }
-
-if (newValue <= MIN_NUMBER) {
+const subtractHandler = () =>{
+   const newValue= parseInt(number.value) - 1;
+   number.value = newValue
+   if (add.disabled === true){
+    add.disabled = false
+   }
+   if (newValue <= MIN_NUMBER){
     subtract.disabled = true
 }
 }
-
 const addHandler = () =>{
-    const newValue = parseInt(number.value) + STEP_AMOUNT
+    const newValue= parseInt(number.value) + 1;
     number.value = newValue
-
     if (subtract.disabled === true){
-        subtract.disabled === false
-    } 
+        subtract.disabled = false
+       }
+    if (newValue >= MAX_NUMBER){
+        add.disabled = true
+    }
 }
-
-subtract.addEventListener('click', substractHandler)
+subtract.addEventListener('click', subtractHandler )
 add.addEventListener('click', addHandler)
