34fc05e862d1ee5f006aca8a164c06ef54a59b17
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-04-03 15:22:51 +0200

    BCL2302_GroupOwen_TshireletsoMpudu_IWA5

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -1,34 +1,54 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let locations = 'RSA'
+let currency = ''
+let customers = null
 
-if location = NAM
-shipping = 600
-else shipping = 800
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * NONE_SELECTED                
+let batteries = 35 * 2
+let pens = 5 * NONE_SELECTED
+const items = shoes + toys + shirts + batteries + pens;
 
-shoes = 300 * 1
-toys - 100 * 5
-hirts = 150 * 'NONE_SELECTED'
-batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED'
 
-shipping = 600
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000) {
-	if (location = NAM && customers < 2) {
-		if (location = RSA)
-			shipping = 0 || calcShipping
+let shipping;
+if (locations === 'RSA') {
+	currency = 'R'
+	if (items < 1000) {
+		shipping = 400;
+		console.log('Price : ', currency, items + shipping)
+	}
+	else if (items >= 1000 && customers === 2) {
+		shipping === 0;
+		console.log('Price : ', currency, items + shipping)
+	}
+	else if (customers !== 1) {
+		console.log(FREE_WARNING)
 	}
 }
 
+if (locations === 'NAM') {
+	currency = '$'
+	if (items < 1000) {
+		shipping = 600;
+		console.log('Price : ', currency, items/17 + shipping)
+	}
+	else if (items >= 1000 && customers === 1) {
+		shipping === 0;
+		console.log('Price : ', currency, (items+ shipping)/17)
+	}
+	else if (customers !== 1) {
+		console.log(FREE_WARNING)
+	}
+}
 
-if (shipping = 0 && customers === 1) { console.log(WARNING) }
+if (locations === 'NK') {
+	console.log(BANNED_WARNING)
+}
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
+
+
