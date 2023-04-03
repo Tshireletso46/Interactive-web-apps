5c87c8a590a446d20965c9c53b83d2dd766abf94
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-04-03 09:42:48 +0200

    BCL2302_GroupOwen_TshireletsoMpudu_IWA5

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -5,27 +5,27 @@ NONE_SELECTED = '0'
 if (location = RSA) { shipping === 400 && currency === 'R' }
 
 if location = NAM
-shipping = 600 
+shipping = 600
 else shipping = 800
 
 shoes = 300 * 1
 toys - 100 * 5
 hirts = 150 * 'NONE_SELECTED'
 batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+pens = 5 * 'NONE_SELECTED'
 
 shipping = 600
 currency = $
 
-if (shoes + batteries + pens + shirts > 1000 ) {
+if (shoes + batteries + pens + shirts > 1000) {
 	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
+		if (location = RSA)
+			shipping = 0 || calcShipping
 	}
+}
 
 
-if (shipping = 0 && customers === 1 ) { console.log(WARNING) }
+if (shipping = 0 && customers === 1) { console.log(WARNING) }
 
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
