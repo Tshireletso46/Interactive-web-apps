40ace20cc6a1fdccc7225ebcaa4b4f6d2f7f4b36
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-04-05 16:11:03 +0200

    BCL2302_GroupOwen_TshireletsoMpudu_IWA6

--- IWA6/challenge1/js/scripts.js
+++ IWA6/challenge1/js/scripts.js
@@ -3,11 +3,15 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-
+/*
+the function !isNaN checks if the string is a number or not, because it picked up o in the primary phone it is running false 
+but because it picked up numbers only in the secondary phone it is running true
+*/
 const primaryValid = !isNaN(primaryPhone)
 const secondaryValid = !isNaN(secondaryPhone)
 
-/*const primaryValid = parseInt(primaryPhone) == (primaryPhone)
+/*Another solution:
+const primaryValid = parseInt(primaryPhone) == (primaryPhone)
 const secondaryValid = parseInt(secondaryPhone) == (secondaryPhone)
 */
 
--- IWA6/challenge2/js/scripts.js
+++ IWA6/challenge2/js/scripts.js
@@ -14,4 +14,7 @@ if (hourOfDay == 00 && minuteOfDay == 00){
 	const balance = startingAfterTax - transport - food - rent
 console.log(balance.toFixed(2))
 }
-	 
+/*
+specifically if hour of the day ==0 and minutes of the day ==00 then i pasersed tax which is a number to /100 which is 0.12
+then * the salary by taxAsDecimal and after minus the product from the salary which will make the balnce 242.05 instead of 274,46
+*/	 
