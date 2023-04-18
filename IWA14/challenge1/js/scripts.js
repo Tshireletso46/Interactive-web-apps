dcec3a89f21ca9fc3c927cebe124dabb6af481f9
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-04-18 15:03:39 +0200

    BCL2302_GroupOwen_TshireletsoMpudu_IWA14

---
+++ IWA14/challenge1/js/scripts.js
@@ -0,0 +1,10 @@
+ const firstName = 'John';
+ const age = 35;
+ const hobby = 'Coding';
+
+const logTwice = () => {
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`);
+}
+
+logTwice()
+logTwice()
